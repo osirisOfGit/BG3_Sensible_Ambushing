a7c5789eb8978ba7f0165f3245688eb222f5fc47
osirisOfGit <osirisofproton@proton.me>
2024-10-15 19:32:58 -0500

    Basic summon logic working - add todos - add pak

---
+++ README.md
@@ -0,0 +1,16 @@
+# TODO
+
+- [ ] Find a way to prevent enemies knowing where stealthed characters were on ambush start
+- [ ] Force surprise on enemies when attacking them
+  - [ ] Before combat starts (exclusive MCM options):
+    - [ ] When stealthed
+    - [ ] When not stealthed and not in sight of enemy?
+    - [ ] When not stealthed and in sight of enemy?
+  - [ ] On crit from stealthed char during combat?
+- [ ] Distance-based eligibility
+  - [ ] Implement Surface when messing with MCM value to show how big the radius is? Or find a more sensible way of doing that
+- [ ] Sight-based eligibility
+- [ ] Teleporting far away party members to the ambush site?
+- [ ] I'm 100% sure there's gonna be multiplayer shenanigans i'll have to deal with
+  - [ ] MCM notification allowing other players to opt in and have rules run?
+- [ ] Flavour notification? "AMBUSH SUCCESSFUL" or something?
--- Sensible_Ambushing/Mods/Sensible_Ambushing/ScriptExtender/Lua/BootstrapServer.lua
+++ Sensible_Ambushing/Mods/Sensible_Ambushing/ScriptExtender/Lua/BootstrapServer.lua
@@ -6,35 +6,90 @@ Ext.Require("Stuff/Utils/_Logger.lua")
 -- https://github.com/FallenStar08/BG3-DUMP/tree/main
 -- https://bg3.norbyte.dev/search?q=Surprised
 
+local function IsCharacterEligibleToJoinAmbush(character)
+    local post_ambush_functions = {}
+
+    if Osi.IsInCombat(character) == 1 then
+        Logger:BasicDebug("Character %s is already in combat", character)
+        return
+    elseif Osi.CanJoinCombat(character) ~= 1 then
+        Logger:BasicWarning("Character %s can't join combat?", character)
+        return
+    end
+
+    if Osi.HasActiveStatus(character, "SNEAKING") == 1 and MCM.Get("SA_sneaking_chars_are_eligible") then
+        Logger:BasicDebug("Character %s is currently sneaking - removing so they can join combat", character)
+        Osi.RemoveStatus(character, "SNEAKING")
+        if MCM.Get("SA_sneaking_chars_get_stealth_back") then
+            table.insert(post_ambush_functions, function(character_to_apply)
+                Logger:BasicDebug("Restoring sneaking to %s", character_to_apply)
+                Osi.ApplyStatus(character_to_apply, "SNEAKING", -1)
+            end)
+        end
+    end
+
+    return post_ambush_functions
+end
+
+local function AreSummonsEligibleToJoinAmbush(character, character_post_ambush_functions)
+    local summon_post_ambush_functions = {}
+
+    local summons = Osi.DB_PlayerSummons:Get(nil)
+    if #summons > 0 then
+        for _, summon in pairs(summons) do
+            summon = summon[1]
+
+            if (Ext.Entity.Get(summon).IsSummon.Summoner.Uuid.EntityUuid == Osi.GetUUID(character)) then
+                summon_post_ambush_functions[summon] = {}
+
+                if MCM.Get("SA_summons_copy_summoner") then
+                    for _, post_ambush_func in pairs(character_post_ambush_functions) do
+                        table.insert(summon_post_ambush_functions[summon], post_ambush_func)
+                    end
+                else
+                    summon_post_ambush_functions[summon] = IsCharacterEligibleToJoinAmbush(summon)
+                end
+            end
+        end
+    end
+
+    return summon_post_ambush_functions
+end
+
 -- Osi.ApplyStatus("S_Player_ShadowHeart_3ed74f06-3c60-42dc-83f6-f034cb47c679", "SNEAKING", -1)
 Ext.Osiris.RegisterListener("CombatStarted", 1, "before", function(combatGuid)
+    local targetEnemy = nil
     for _, character in pairs(Osi.DB_Players:Get(nil)) do
         local character = character[1]
-        if Osi.IsInCombat(character) ~= 1 then
-            if Osi.CanJoinCombat(character) ~= 1 then
-                Logger:BasicWarning("Player %s can't join combat?", character)
-            else
-                local restoreSneaking = false
-                if Osi.HasActiveStatus(character, "SNEAKING") == 1 and MCM.Get("sneaking_chars_are_eligible") then
-                    Logger:BasicDebug("Party member %s is currently sneaking - temporarily removing so they can join combat", character)
-                    Osi.RemoveStatus(character, "SNEAKING")
-                    restoreSneaking = true
-                end
+        local character_post_ambush_functions = IsCharacterEligibleToJoinAmbush(character)
+
+        if character_post_ambush_functions then
+            local summons_and_post_ambush_functions = AreSummonsEligibleToJoinAmbush(character, character_post_ambush_functions)
+
+            if not targetEnemy then
                 for _, combatRow in pairs(Osi.DB_Is_InCombat:Get(nil, combatGuid)) do
                     local combatChar = combatRow[1]
                     if Osi.IsPlayer(combatChar) ~= 1 then
-                        Osi.EnterCombat(character, combatChar)
-
-                        if restoreSneaking then
-                            Logger:BasicDebug("Restoring sneaking to %s", character)
-                            Osi.ApplyStatus(character, "SNEAKING", -1)
-                        end
+                        targetEnemy = combatChar
                     end
+                end
+            end
+
+            Osi.EnterCombat(character, targetEnemy)
+
+            for _, post_ambush_func in pairs(character_post_ambush_functions) do
+                post_ambush_func(character)
+            end
+
+            for summon, post_ambush_functions in pairs(summons_and_post_ambush_functions) do
+                for _, post_ambush_func in pairs(post_ambush_functions) do
+                    post_ambush_func(summon)
                 end
             end
         end
     end
 end)
+
 
 --[[
 local function OnCombatStarted(combatGUID)
@@ -54,7 +109,7 @@ local function OnCombatStarted(combatGUID)
             -- to call Ext.Entity.Get on participantEntity.CombatParticipant.CombatHandle first, then
             -- adjust the properties on that.
             --participantEntity.CombatParticipant.CombatHandle.CombatState.Initiatives[participantTpl] = initiativeOrder
-c
+
             _P("Character: " .. participantTpl)
             _P("Initiative: " .. initiativeOrder .. "\n")
 
