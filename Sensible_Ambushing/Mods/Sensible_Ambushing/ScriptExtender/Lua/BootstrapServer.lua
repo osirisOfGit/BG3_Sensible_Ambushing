87fa8b48ef59772ea85355de4867e523726d0130
osirisofgit <github.com.j95fw@passmail.net>
2024-10-19 16:16:40 -0500

    Got running working better

--- Sensible_Ambushing/Mods/Sensible_Ambushing/ScriptExtender/Lua/BootstrapServer.lua
+++ Sensible_Ambushing/Mods/Sensible_Ambushing/ScriptExtender/Lua/BootstrapServer.lua
@@ -2,4 +2,6 @@ Ext.Require("Stuff/Utils/_FileUtils.lua")
 Ext.Require("Stuff/Utils/_ModUtils.lua")
 Ext.Require("Stuff/Utils/_Logger.lua")
 
+Ext.Require("Extras/_MetalProne.lua")
+
 Ext.Require("_AmbushDirector.lua")
