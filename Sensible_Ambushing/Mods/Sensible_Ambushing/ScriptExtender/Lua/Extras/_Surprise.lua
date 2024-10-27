ad1359dc8083b4537e88c23cc6c011015e5eece9
osirisofgit <github.com.j95fw@passmail.net>
2024-10-27 15:47:45 -0500

    whoops

--- Sensible_Ambushing/Mods/Sensible_Ambushing/ScriptExtender/Lua/Extras/_Surprise.lua
+++ Sensible_Ambushing/Mods/Sensible_Ambushing/ScriptExtender/Lua/Extras/_Surprise.lua
@@ -1,4 +1,6 @@
 -- Maps to Sensible_Ambushing\Public\Sensible_Ambushing\DifficultyClasses\DifficultyClasses.lsx
+Extra_Surprise = {}
+
 Extra_Surprise.difficultyClassUUIDs = {
 	[1] = "a1b2c3d4-e5f6-7a8b-9c0d-e1f2a3b4c5d6",
 	[2] = "b2c3d4e5-f6a7-8b9c-0d1e-2f3a4b5c6d7e",
